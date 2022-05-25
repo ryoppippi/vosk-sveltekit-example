33330a0c439db182d95c770e648d7eaadabb361a
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2022-05-25 11:52:26 +0100

    add config for github actions

--- svelte.config.js
+++ svelte.config.js
@@ -1,33 +1,40 @@
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
-import path from 'path';
+import adapter from "@sveltejs/adapter-static";
+import preprocess from "svelte-preprocess";
+import path from "path";
+
+const dev = process.env.NODE_ENV === "development";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  preprocess: preprocess({
+    postcss: true,
+  }),
 
   kit: {
     adapter: adapter({
       // default options are shown
-      pages: 'build',
-      assets: 'build',
+      pages: "build",
+      assets: "build",
       fallback: null,
       precompress: true,
     }),
     prerender: {
       default: true,
     },
-
+    paths: {
+      base: dev ? "" : "/vosk-sveltekit-example",
+    },
+    appDir: "internal",
     vite: {
       define: {
         global: {},
       },
       resolve: {
         alias: {
-          $components: path.resolve('./src/components'),
-          $lib: path.resolve('./src/lib'),
+          $components: path.resolve("./src/components"),
+          $lib: path.resolve("./src/lib"),
         },
       },
     },
